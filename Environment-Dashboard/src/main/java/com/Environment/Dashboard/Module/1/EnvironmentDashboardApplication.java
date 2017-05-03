feb30697010549665791b13d30585e96e8236f29
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-05-03 14:26:59 +0300

    Created project.

---
+++ Environment-Dashboard/src/main/java/com/Environment/Dashboard/Module/1/EnvironmentDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.Environment.Dashboard.Module.1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentDashboardApplication.class, args);
+	}
+}
