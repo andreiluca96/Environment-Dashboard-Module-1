f60ec8dcd6d8398145fd527d08cd4535e5213c15
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-05-03 18:14:10 +0300

    Added basic event class.

--- EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Event.java
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Event.java
@@ -1,7 +1,13 @@
 package com.EnvironmentDashboardModule1.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Luca Andrei on 5/3/2017.
  */
+@Entity
+@Table(name = "Event")
 public class Event {
+    String name;
 }
