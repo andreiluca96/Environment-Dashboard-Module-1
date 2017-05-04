a524a906900f83743180f4f4c9aca716f4e9c36c
vladutc <vladutcojocariu@yahoo.com>
2017-05-05 00:25:46 +0300

    Add files via upload
    Cateva teste pentru DTO - in unele cazuri trebuie completate niste validari pentru valori negative ca sa treaca testele.

---
+++ EnvironmentDashboard/src/test/java/com/EnvironmentDashboardModule1/DTO/CreatingTornadoDtoTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.EnvironmentDashboardModule1.DTO;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Vladut
+ */
+public class CreatingTornadoDtoTest {
+    
+
+    @Test
+    public void testGetWindspeed() {
+        System.out.println("getWindspeed");
+        CreatingTornadoDto instance = new CreatingTornadoDto();
+        Double expResult = instance.windspeed;
+        Double result = instance.getWindspeed();
+        assertEquals(expResult, result);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testSetWindspeed() {
+        System.out.println("setWindspeed");
+        Double windspeed = -1.2;
+        CreatingTornadoDto instance = new CreatingTornadoDto();
+        instance.setWindspeed(windspeed);
+        fail("The test case is a prototype.");
+    }
+    
+}
