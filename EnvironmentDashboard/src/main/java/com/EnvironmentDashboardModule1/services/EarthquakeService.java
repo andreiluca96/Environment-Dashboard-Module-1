c97ad17cccd63f488c381b5c99bdfe5dfbf870d0
Sfarghiu <sfarghiuandrei@gmail.com>
2017-05-04 20:30:52 +0300

    Add files via upload

---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/services/EarthquakeService.java
@@ -0,0 +1,9 @@
+package com.EnvironmentDashboardModule1.services;
+
+import com.EnvironmentDashboardModule1.models.Earthquake.;
+
+/**
+ * Created by Sfarghiu Andrei on 5/4/2017.
+ */
+public interface EarthquakeService extends CrudService<Earthquake> {
+}
