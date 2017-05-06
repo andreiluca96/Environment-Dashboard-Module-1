91d75752d42c52324691dea2773f8ece0ed0d5b7
Dragos AIOANEI <daioanei@ias.bitdefender.biz>
2017-05-06 21:21:14 +0300

    EventBuilder validation methods added

--- EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Builders/EventBuilder.java
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Builders/EventBuilder.java
@@ -29,46 +29,55 @@ public class EventBuilder {
     protected Double radius;
 
     public EventBuilder setName(String name) {
+        validateName(name);
         this.name = name;
         return this;
     }
 
     public EventBuilder setLatitude(Double latitude) {
+        validateLatitude(latitude);
         this.latitude = latitude;
         return this;
     }
 
     public EventBuilder setLongitude(Double longitude) {
+        validateLongitude(longitude);
         this.longitude = longitude;
         return this;
     }
 
     public EventBuilder setStartingTime(Date startingTime) {
+        validateTime(startingTime);
         this.startingTime = startingTime;
         return this;
     }
 
     public EventBuilder setEndingTime(Date endingTime) {
+        validateTime(endingTime);
         this.endingTime = endingTime;
         return this;
     }
 
     public EventBuilder setSeverity(String severity) {
+        validateSeverity(severity);
         this.severity = severity;
         return this;
     }
 
     public EventBuilder setDescription(String description) {
+        validateName(description);
         this.description = description;
         return this;
     }
 
     public EventBuilder setHints(String hints) {
+        validateName(hints);
         this.hints = hints;
         return this;
     }
 
     public EventBuilder setRadius(Double radius) {
+        validateRadius(radius);
         this.radius = radius;
         return this;
     }
@@ -87,5 +96,55 @@ public class EventBuilder {
         event.setStartingTime(startingTime);
 
         return event;
+    }
+
+
+    //Dragos -> validate severity
+    private void validateSeverity(String severity) {
+        if (!severity.equals("GREEN") && !severity.equals("YELLOW") && !severity.equals("ORANGE") && !severity.equals("RED")) {
+            throw new IllegalArgumentException(severity);
+        }
+    }
+
+    //Dragos -> validate latitude
+    private void validateLatitude(Double latitude) {
+        if (latitude < -85 || latitude > 85) {
+            throw new IllegalArgumentException(Double.toString(latitude));
+        }
+    }
+
+    //Dragos -> validate longitude
+    private void validateLongitude(Double longitude) {
+        if (longitude < -180 || longitude > 180) {
+            throw new IllegalArgumentException(Double.toString(longitude));
+        }
+    }
+
+    //Dragos -> validate id
+    private void validateId(Integer id) {
+        if (id < 0) {
+            throw new IllegalArgumentException(Double.toString(id));
+        }
+    }
+
+    //Dragos -> validate name
+    private void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException(name);
+        }
+    }
+
+    //Dragos -> validate time
+    private void validateTime(Date time) {
+        if (time == null) {
+            throw new IllegalArgumentException("null time");
+        }
+    }
+
+    //Dragos -> validate radius
+    private void validateRadius(Double radius) {
+        if (radius < 0) {
+            throw new IllegalArgumentException(Double.toString(radius));
+        }
     }
 }
