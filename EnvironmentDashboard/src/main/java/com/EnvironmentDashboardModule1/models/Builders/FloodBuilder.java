aa340beea2530abe7416862d4e8e3523718d6cb1
Dragos AIOANEI <daioanei@ias.bitdefender.biz>
2017-05-07 19:48:39 +0300

    Added FloodBuilder

---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Builders/FloodBuilder.java
@@ -0,0 +1,92 @@
+package com.EnvironmentDashboardModule1.models.Builders;
+
+import com.EnvironmentDashboardModule1.models.Events.Flood;
+
+import java.util.Date;
+
+/**
+ * Created by daioanei on 5/4/2017.
+ */
+
+public class FloodBuilder extends EventBuilder{
+
+    private Double precipitationLevel;
+
+    public FloodBuilder setName(String name) {
+        validateString(name);
+        this.name = name;
+        return this;
+    }
+
+    public FloodBuilder setLatitude(Double latitude) {
+        validateLatitude(latitude);
+        this.latitude = latitude;
+        return this;
+    }
+
+    public FloodBuilder setLongitude(Double longitude) {
+        validateLongitude(longitude);
+        this.longitude = longitude;
+        return this;
+    }
+
+    public FloodBuilder setStartingTime(Date startingTime) {
+        validateTime(startingTime);
+        this.startingTime = startingTime;
+        return this;
+    }
+
+    public FloodBuilder setEndingTime(Date endingTime) {
+        validateTime(endingTime);
+        this.endingTime = endingTime;
+        return this;
+    }
+
+    public FloodBuilder setSeverity(String severity) {
+        validateSeverity(severity);
+        this.severity = severity;
+        return this;
+    }
+
+    public FloodBuilder setDescription(String description) {
+        validateString(description);
+        this.description = description;
+        return this;
+    }
+
+    public FloodBuilder setHints(String hints) {
+        validateString(hints);
+        this.hints = hints;
+        return this;
+    }
+
+    public FloodBuilder setRadius(Double radius) {
+        validateNonNegativeValue(radius);
+        this.radius = radius;
+        return this;
+    }
+
+    public FloodBuilder setPrecipitationLevel(Double precipitationLevel) {
+        validateNonNegativeValue(precipitationLevel);
+        this.precipitationLevel = precipitationLevel;
+        return this;
+    }
+
+    public Flood getFlood(){
+        Flood flood = new Flood();
+
+        flood.setLatitude(latitude);
+        flood.setLongitude(longitude);
+        flood.setDescription(description);
+        flood.setEndingTime(endingTime);
+        flood.setHints(hints);
+        flood.setName(name);
+        flood.setRadius(radius);
+        flood.setSeverity(severity);
+        flood.setStartingTime(startingTime);
+        flood.setPrecipitationLevel(precipitationLevel);
+
+        return flood;
+    }
+
+}
