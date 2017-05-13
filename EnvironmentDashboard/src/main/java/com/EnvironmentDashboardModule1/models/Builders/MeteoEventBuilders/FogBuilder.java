6f1c57e3f5dff9faf9cc3553e7ef0d8e7da370e8
Sfarghiu <sfarghiuandrei@gmail.com>
2017-05-13 13:26:54 +0300

    Add files via upload

---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Builders/MeteoEventBuilders/FogBuilder.java
@@ -0,0 +1,115 @@
+package com.EnvironmentDashboardModule1.models.Builders.MeteoEventBuilders;
+
+import com.EnvironmentDashboardModule1.models.MeteoEvents.Fog;
+
+import java.util.Date;
+
+/**
+ * Created by Andrei Sfarghiu on 5/13/2017.
+ */
+
+
+public class FogBuilder extends MeteoEventBuilder{
+
+    private Integer temperature;
+
+    private Integer humidity;
+
+    private Integer precipitationLevel;
+
+    public FogBuilder setName(String name) {
+        validateString(name);
+        this.name = name;
+        return this;
+    }
+
+    public FogBuilder setLatitude(Double latitude) {
+        validateLatitude(latitude);
+        this.latitude = latitude;
+        return this;
+    }
+
+    public FogBuilder setLongitude(Double longitude) {
+        validateLongitude(longitude);
+        this.longitude = longitude;
+        return this;
+    }
+
+    public FogBuilder setStartingTime(Date startingTime) {
+        validateTime(startingTime);
+        this.startingTime = startingTime;
+        return this;
+    }
+
+    public FogBuilder setEndingTime(Date endingTime) {
+        validateTime(endingTime);
+        this.endingTime = endingTime;
+        return this;
+    }
+
+    public FogBuilder setSeverity(String severity) {
+        validateSeverity(severity);
+        this.severity = severity;
+        return this;
+    }
+
+    public FogBuilder setDescription(String description) {
+        validateString(description);
+        this.description = description;
+        return this;
+    }
+
+    public FogBuilder setHints(String hints) {
+        validateString(hints);
+        this.hints = hints;
+        return this;
+    }
+
+    public FogBuilder setRadius(Double radius) {
+        validateNonNegativeValue(radius);
+        this.radius = radius;
+        return this;
+    }
+
+    public FogBuilder setTemperature(Integer temperature) {
+        this.temperature = temperature;
+        return this;
+    }
+
+    public FogBuilder setHumidity(Integer humidity) {
+        validateNonNegativeValue(humidity);
+        this.humidity = humidity;
+        return this;
+    }
+
+    public FogBuilder setPrecipitationLevel(Integer precipitationLevel) {
+        validateNonNegativeValue(precipitationLevel);
+        this.precipitationLevel = precipitationLevel;
+        return this;
+    }
+
+
+    public Fog getFog() {
+        Fog fog=new Fog();
+        fog.setLatitude(latitude);
+        fog.setLongitude(longitude);
+        fog.setDescription(description);
+        fog.setEndingTime(endingTime);
+        fog.setHints(hints);
+        fog.setName(name);
+        fog.setRadius(radius);
+        fog.setSeverity(severity);
+        fog.setStartingTime(startingTime);
+        fog.setHumidity(humidity);
+        fog.setPrecipitationLevel(precipitationLevel);
+        fog.setTemperature(temperature);
+        return fog;
+    }
+
+    
+    protected void validateNonNegativeValue(Integer value) {
+        if (value < 0) {
+            throw new IllegalArgumentException(Integer.toString(value));
+        }
+    }
+}
---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/models/Builders/MeteoEventBuilders/RainBuilder.java
@@ -0,0 +1,115 @@
+package com.EnvironmentDashboardModule1.models.Builders.MeteoEventBuilders;
+
+import com.EnvironmentDashboardModule1.models.MeteoEvents.Rain;
+
+import java.util.Date;
+
+/**
+ * Created by Andrei Sfarghiu on 5/13/2017.
+ */
+
+
+public class RainBuilder extends MeteoEventBuilder{
+
+    private Integer temperature;
+
+    private Integer humidity;
+
+    private Integer precipitationLevel;
+
+    public RainBuilder setName(String name) {
+        validateString(name);
+        this.name = name;
+        return this;
+    }
+
+    public RainBuilder setLatitude(Double latitude) {
+        validateLatitude(latitude);
+        this.latitude = latitude;
+        return this;
+    }
+
+    public RainBuilder setLongitude(Double longitude) {
+        validateLongitude(longitude);
+        this.longitude = longitude;
+        return this;
+    }
+
+    public RainBuilder setStartingTime(Date startingTime) {
+        validateTime(startingTime);
+        this.startingTime = startingTime;
+        return this;
+    }
+
+    public RainBuilder setEndingTime(Date endingTime) {
+        validateTime(endingTime);
+        this.endingTime = endingTime;
+        return this;
+    }
+
+    public RainBuilder setSeverity(String severity) {
+        validateSeverity(severity);
+        this.severity = severity;
+        return this;
+    }
+
+    public RainBuilder setDescription(String description) {
+        validateString(description);
+        this.description = description;
+        return this;
+    }
+
+    public RainBuilder setHints(String hints) {
+        validateString(hints);
+        this.hints = hints;
+        return this;
+    }
+
+    public RainBuilder setRadius(Double radius) {
+        validateNonNegativeValue(radius);
+        this.radius = radius;
+        return this;
+    }
+
+    public RainBuilder setTemperature(Integer temperature) {
+        this.temperature = temperature;
+        return this;
+    }
+
+    public RainBuilder setHumidity(Integer humidity) {
+        validateNonNegativeValue(humidity);
+        this.humidity = humidity;
+        return this;
+    }
+
+    public RainBuilder setPrecipitationLevel(Integer precipitationLevel) {
+        validateNonNegativeValue(precipitationLevel);
+        this.precipitationLevel = precipitationLevel;
+        return this;
+    }
+
+
+    public Rain getRain() {
+        Rain rain=new Rain();
+        rain.setLatitude(latitude);
+        rain.setLongitude(longitude);
+        rain.setDescription(description);
+        rain.setEndingTime(endingTime);
+        rain.setHints(hints);
+        rain.setName(name);
+        rain.setRadius(radius);
+        rain.setSeverity(severity);
+        rain.setStartingTime(startingTime);
+        rain.setHumidity(humidity);
+        rain.setPrecipitationLevel(precipitationLevel);
+        rain.setTemperature(temperature);
+        return rain;
+    }
+
+    
+    protected void validateNonNegativeValue(Integer value) {
+        if (value < 0) {
+            throw new IllegalArgumentException(Integer.toString(value));
+        }
+    }
+}
