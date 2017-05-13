d3b489f94a6e96e3edc1d30192049d504301be5b
radulungu <radu_lungu97@yahoo.com>
2017-05-13 14:43:26 +0300

    Add files via upload

---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/DTO/Interfaces/CanicularWeatherInterfaces/ILatitude.java
@@ -0,0 +1,8 @@
+package com.EnvironmentDashboardModule1.DTO.Interfaces.CanicularWeatherInterfaces;
+
+/**
+ * Created by Radu Lungu on 5/13/2017.
+ */
+public interface ILatitude {
+    ILongitude latitude(Double latitude);
+}
---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/DTO/Interfaces/CanicularWeatherInterfaces/IRadius.java
@@ -0,0 +1,9 @@
+package com.EnvironmentDashboardModule1.DTO.Interfaces.CanicularWeatherInterfaces;
+
+
+/**
+ * Created by Radu Lungu on 5/13/2017.
+ */
+public interface IRadius {
+    IPrecipitationLevel radius(Double radius);
+}
