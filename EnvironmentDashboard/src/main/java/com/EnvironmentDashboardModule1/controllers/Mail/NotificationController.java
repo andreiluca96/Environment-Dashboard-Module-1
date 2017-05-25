c089e9435ba01073d109de3e2d2a7a63dd55ec3a
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-05-25 19:29:35 +0300

    Finished email service notification for current events with partial format.

--- EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/controllers/Mail/NotificationController.java
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/controllers/Mail/NotificationController.java
@@ -4,8 +4,10 @@ package com.EnvironmentDashboardModule1.controllers.Mail;
  * Created by Luca Andrei on 5/25/2017.
  */
 
+import com.EnvironmentDashboardModule1.models.EventMapping;
 import com.EnvironmentDashboardModule1.models.Events.Event;
 import com.EnvironmentDashboardModule1.models.Users.User;
+import com.EnvironmentDashboardModule1.services.EventMappingService;
 import com.EnvironmentDashboardModule1.services.EventService;
 import com.EnvironmentDashboardModule1.services.Mail.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +24,13 @@ import java.util.List;
 @RequestMapping("v1/notification")
 public class NotificationController {
     @Autowired
-    NotificationService notificationService;
+    private NotificationService notificationService;
 
     @Autowired
-    EventService eventService;
+    private EventService eventService;
+
+    @Autowired
+    private EventMappingService eventMappingService;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public void sendTestMail() {
@@ -37,16 +42,43 @@ public class NotificationController {
         List<Event> eventList = eventService.getAll();
         for (Event event : eventList) {
             Date currentDate = new Date();
-            System.out.println(currentDate);
             if (event.getStartingTime().before(currentDate) && event.getEndingTime().after(currentDate)) {
-                System.out.println("Am gasit un event cu data corespunzatoare");
-                RestTemplate restTemplate = new RestTemplate();
-                User[] users = restTemplate.getForObject("http://localhost:8100/v1/users/" + event.getLatitude() + "/" + event.getLongitude() + "/" + event.getRadius(), User[].class);
-                for (User user : users) {
-                    notificationService.sendMail(user.getEmail(), "New event just happened!", event.toString());
-                }
+                User[] users = getUsersInRange(event);
+
+                sendEmailToUsersWithEvent(users, event);
             }
         }
+    }
+
+    private void sendEmailToUsersWithEvent(User[] users, Event event) {
+        String eventType  = eventMappingService.getById(event.getId()).getEventType();
+        String title = "";
+        String content = "";
+
+        if (eventType.equals("Event")) {
+            title = "A new event just happened!";
+            content = "A new event just happened in one of your locations.\n";
+            content += "Severity: " + event.getSeverity() + "\n";
+            content += "Description: " + event.getDescription() + "\n";
+            content += "We recommend you to " + event.getHints() + "\n";
+            content += "Starting time: " + event.getStartingTime() + "\n";
+            content += "Ending time: " + event.getEndingTime() + "\n";
+
+            content += "\n\n\n";
+
+            content += "Take care of you.\nEnvironment dashboard.";
+        }
+
+        for (User user : users) {
+            notificationService.sendMail(user.getEmail(), title, content);
+        }
+    }
+
+    private User[] getUsersInRange(Event event) {
+        RestTemplate restTemplate = new RestTemplate();
+        User[] users = restTemplate.getForObject("http://localhost:8100/v1/users/" + event.getLatitude() + "/" + event.getLongitude() + "/" + event.getRadius(), User[].class);
+
+        return users;
     }
 
 }
