24e7aaf2938a2f4f05498182a26bfe0e47d1c19e
sabisav <sabisav@yahoo.com>
2017-05-28 22:40:27 +0300

    Updated email notification format

--- EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/controllers/Mail/NotificationController.java
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/controllers/Mail/NotificationController.java
@@ -16,10 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.*;
 
 @RestController
 @RequestMapping("v1/notification")
@@ -33,30 +37,86 @@ public class NotificationController {
     @Autowired
     private EventMappingService eventMappingService;
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public void sendTestMail() {
-        notificationService.sendMail("luca.andrei96@gmail.com", "Un NOU EVENIMENT!!!", "FUGGGIIITTTZZZ!!!");
+    @RequestMapping(value = "/test/{id}", method = RequestMethod.GET)
+    public void sendTestMail(@PathVariable Long id) throws MessagingException {
+        Event event = eventService.getById(id);
+        String eventType = eventMappingService.getById(event.getId()).getEventType();
+        String title = "";
+        String content = "";
+        String headerColor = "";
+        String severityColor = "";
+        StringBuilder message = new StringBuilder();
+
+        if (eventType.equals("Event")) {
+            title = "A new event just happened!";
+            if (event.getSeverity().equals("RED")) {
+                headerColor = "<tr bgcolor=\"#DC143C\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color='#DC143C'><b>RED</b></font>";
+            } else if (event.getSeverity().equals("GREEN")) {
+                headerColor = "<tr bgcolor=\"#83af9a\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color='#83af9a'><b>GREEN</b></font>";
+            } else if (event.getSeverity().equals("YELLOW")) {
+                headerColor = "<tr bgcolor=\"#FFDF00\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color='#FFDF00'><b>YELLOW</b></font>";
+            } else if (event.getSeverity().equals("ORANGE")) {
+                headerColor = "<tr bgcolor=\"#FFA07A\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color= '#FFA07A'><b>ORANGE</b></font>";
+            }
+
+            message.append("<table style=\"margin: 0px auto\">");
+            message.append(headerColor);
+            message.append("<td style='padding: 10px 30px 0px;'>");
+            content = "<p style=\"font-size:22px\"><b>A new event just happened in one of your locations</b></p><br>";
+            message.append(content);
+            message.append("</td>");
+            content = "";
+            content += severityColor;
+            message.append("<tr bgcolor=\"#F5F5F5\">");
+
+            content += "<br>Description: " + event.getDescription();
+            content += "<br>We recommend you to: " + event.getHints();
+            content += "<br>Starting time: " + event.getStartingTime();
+            content += "<br>Ending time: " + event.getEndingTime();
+            content += "<br><br><i>Stay safe.</i><br>Environment Dashboard";
+
+            message.append("<td style='padding:30px'>");
+            message.append(content);
+            message.append("</td>");
+            message.append("<tr>");
+            message.append("</table>");
+            content = message.toString();
+        }
+
+        notificationService.sendMail("sabisav@yahoo.com", title, content);
     }
 
     @RequestMapping(value = "/current-event-notification", method = RequestMethod.GET)
-    public void notifyUsers() {
+    public void notifyUsers() throws MessagingException {
         List<Event> eventList = eventService.getAll();
         for (Event event : eventList) {
             Date currentDate = new Date();
             if (event.getStartingTime().before(currentDate) && event.getEndingTime().after(currentDate)) {
                 User[] users = getUsersInRange(event);
 
-                sendEmailToUsersWithEvent(users, event);
+                try {
+                    sendEmailToUsersWithEvent(users, event);
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     @RequestMapping(value = "/event/{id}", method = RequestMethod.GET)
-    public void notifyUsersForEvent(@PathVariable Long id) {
+    public void notifyUsersForEvent(@PathVariable Long id) throws MessagingException {
         Event event = eventService.getById(id);
         User[] users = getUsersInRange(event);
 
-        sendEmailToUsersWithEvent(users, event);
+        try {
+            sendEmailToUsersWithEvent(users, event);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping(value = "/notification-daemon", method = RequestMethod.GET)
@@ -73,10 +133,14 @@ public class NotificationController {
 
                 for (Event event : events) {
                     Date currentDate = new Date();
-                    if ((notifiedEvent.get(event.getId()) == null || notifiedEvent.get(event.getId()).equals(Boolean.FALSE))  &&
-                        event.getStartingTime().before(currentDate) &&
-                        event.getEndingTime().after(currentDate)) {
-                        notifyUsersForEvent(event.getId());
+                    if ((notifiedEvent.get(event.getId()) == null || notifiedEvent.get(event.getId()).equals(Boolean.FALSE)) &&
+                            event.getStartingTime().before(currentDate) &&
+                            event.getEndingTime().after(currentDate)) {
+                        try {
+                            notifyUsersForEvent(event.getId());
+                        } catch (MessagingException e) {
+                            e.printStackTrace();
+                        }
                         notifiedEvent.put(event.getId(), Boolean.TRUE);
                     }
                 }
@@ -84,23 +148,52 @@ public class NotificationController {
         }).start();
     }
 
-    private void sendEmailToUsersWithEvent(User[] users, Event event) {
-        String eventType  = eventMappingService.getById(event.getId()).getEventType();
+    private void sendEmailToUsersWithEvent(User[] users, Event event) throws MessagingException {
+        String eventType = eventMappingService.getById(event.getId()).getEventType();
         String title = "";
         String content = "";
+        String headerColor = "";
+        String severityColor = "";
+        StringBuilder message = new StringBuilder();
 
         if (eventType.equals("Event")) {
             title = "A new event just happened!";
-            content = "A new event just happened in one of your locations.\n";
-            content += "Severity: " + event.getSeverity() + "\n";
-            content += "Description: " + event.getDescription() + "\n";
-            content += "We recommend you to " + event.getHints() + "\n";
-            content += "Starting time: " + event.getStartingTime() + "\n";
-            content += "Ending time: " + event.getEndingTime() + "\n";
+            if (event.getSeverity().equals("RED")) {
+                headerColor = "<tr bgcolor=\"#DC143C\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color='#DC143C'><b>RED</b></font>";
+            } else if (event.getSeverity().equals("GREEN")) {
+                headerColor = "<tr bgcolor=\"#83af9a\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color='#83af9a'><b>GREEN</b></font>";
+            } else if (event.getSeverity().equals("YELLOW")) {
+                headerColor = "<tr bgcolor=\"#FFDF00\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color='#FFDF00'><b>YELLOW</b></font>";
+            } else if (event.getSeverity().equals("ORANGE")) {
+                headerColor = "<tr bgcolor=\"#FFA07A\" style='text-align:center;color:white'>";
+                severityColor = "Severity: <font color= '#FFA07A'><b>ORANGE</b></font>";
+            }
 
-            content += "\n\n\n";
+            message.append("<table style=\"margin: 0px auto\">");
+            message.append(headerColor);
+            message.append("<td style='padding: 10px 30px 0px;'>");
+            content = "<p style=\"font-size:22px\"><b>A new event just happened in one of your locations</b></p><br>";
+            message.append(content);
+            message.append("</td>");
+            content = "";
+            content += severityColor;
+            message.append("<tr bgcolor=\"#F5F5F5\">");
 
-            content += "Take care of you.\nEnvironment dashboard.";
+            content += "<br>Description: " + event.getDescription();
+            content += "<br>We recommend you to: " + event.getHints();
+            content += "<br>Starting time: " + event.getStartingTime();
+            content += "<br>Ending time: " + event.getEndingTime();
+            content += "<br><br><i>Stay safe.</i><br>Environment Dashboard";
+
+            message.append("<td style='padding:30px'>");
+            message.append(content);
+            message.append("</td>");
+            message.append("<tr>");
+            message.append("</table>");
+            content = message.toString();
         }
 
         for (User user : users) {
--- EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/services/Mail/NotificationService.java
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/services/Mail/NotificationService.java
@@ -1,9 +1,16 @@
 package com.EnvironmentDashboardModule1.services.Mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 /**
  * Created by Luca Andrei on 5/25/2017.
@@ -11,9 +18,46 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
-    @Autowired
-    private JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private String port;
+
+    public void sendMail(String email, String subject, String content) throws MessagingException {
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        };
+
+        Session session = Session.getInstance(properties,auth);
+        Message mail = new MimeMessage(session);
+        InternetAddress[] toAddresses = { new InternetAddress(email) };
+        mail.setRecipients(Message.RecipientType.TO, toAddresses);
+        mail.setFrom(new InternetAddress("environment.dashboard@gmail.com"));
+        mail.setSubject(subject);
+        mail.setContent(content,"text/html");
+
+        Transport.send(mail);
+    }
+
+    /*@Autowired
+    private JavaMailSender javaMailSender;
     public void sendMail(String email, String subject, String content) {
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(email);
@@ -22,5 +66,6 @@ public class NotificationService {
         mail.setText(content);
 
         javaMailSender.send(mail);
-    }
+    }*/
+
 }
