44a1dd925c0f97cf1c6e4d960c929d737a48e762
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-05-31 13:29:48 +0300

    Added interfaces for DTOs builders.

---
+++ EnvironmentDashboard/src/main/java/com/EnvironmentDashboardModule1/DTO/Interfaces/MeteoEventInterface/IUserId.java
@@ -0,0 +1,8 @@
+package com.EnvironmentDashboardModule1.DTO.Interfaces.MeteoEventInterface;
+
+/**
+ * Created by Luca Andrei on 5/31/2017.
+ */
+public interface IUserId {
+    IId userId(String userId);
+}
